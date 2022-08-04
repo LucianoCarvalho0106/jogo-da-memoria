23229a0bbb0ffe7b6769e2619065f688373285ff
Luciano <carvalholuciano232@gmail.com>
2022-08-04 20:05:53 -0300

    terminei a pagina de login

---
+++ scripts/login.js
@@ -0,0 +1,23 @@
+const nome = document.querySelector(".text");
+const btn = document.querySelector(".btn-enviar");
+const form = document.querySelector(".form-login");
+
+const checarNome = ({target})=>{
+
+    
+    if(target.value.length >2 ){
+        btn.removeAttribute("disabled");
+        return;
+    }else{
+        btn.setAttribute("disabled","");
+    }
+}
+
+const quandoEnviar = (e)=>{
+    e.preventDefault();
+    localStorage.setItem("Jogador",nome.value);
+    window.location = "paginas/jogo.html"
+}
+
+nome.addEventListener("input",checarNome);
+form.addEventListener("submit",quandoEnviar)
